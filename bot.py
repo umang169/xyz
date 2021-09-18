541c7e893af0a0d26560ba421dab90bf7cada0c6
Manjeetxtyy <90960040+Manjeetxtyy@users.noreply.github.com>
2021-09-18 14:59:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ bot = Client('pdiskshortner bot',
 async def start(bot, message):
     await message.reply(
         f"**Hi {message.chat.first_name}!**\n\n"
-        "I'm Pdisk link converter bot. Just send me link and get converted link of pdisk.\n\n Created By @steallootdeal")
+        "I'm Pdisk link converter bot. Just send me link and get converted link of pdisk.\n\n Created By @MovieHubNewHD2")
 
 @bot.on_message(filters.command('help') & filters.private)
 async def help(bot, message):
@@ -49,7 +49,7 @@ async def link_handler(bot, message):
 
 async def post_shortlink(Link):
     url = 'http://linkapi.net/open/create_item'
-    params = {'api_key': API_KEY, 'content_src': Link, 'link_type': 'link', 'cover_url' : '/steallootdeal.jpeg'}
+    params = {'api_key': API_KEY, 'content_src': Link, 'link_type': 'link'}
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params, raise_for_status=True) as response:
             data = await response.json()
