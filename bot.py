79efae151eddecd39bfd94c999b7fb1b70b097c4
umang169 <90959310+umang169@users.noreply.github.com>
2021-09-29 19:55:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from pyrogram.handlers import MessageHandler
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
-API_KEY = environ.get('API_KEY', '3494ad69b7c32aa52e71adf0da498357')
+API_KEY = environ.get('API_KEY', '89b3e9e8171de4fe21a3b56f16f8573c')
 
 bot = Client('pdiskshortner bot',
              api_id=API_ID,
